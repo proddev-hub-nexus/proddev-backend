c58b1f147b022ede5deb2e829b302ab9e7ac2e65
Olude David <odavidolumide@gmail.com>
2025-08-02 16:59:25 +0200

    comment to test pat

--- backend/utils/rate_limiter.py
+++ backend/utils/rate_limiter.py
@@ -1,5 +1,5 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-# Create limiter instance globally
+# Create limiter instance globally to limit request attacks
 limiter = Limiter(key_func=get_remote_address)
